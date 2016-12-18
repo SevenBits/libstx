c32d1444ef6c425e892365c36bbbad001759ce64
Todd Gaunt <toddgaunt@protonmail.ch>
2016-12-18 16:46:59 -0500

    Add -O1 optimization compiler flag

--- config.mk
+++ config.mk
@@ -6,7 +6,7 @@ PREFIX=/usr/local
 MANPREFIX=${PREFIX}/share/man
 
 # Compilation flags
-CFLAGS=-g -std=c99 -pedantic -Wall -Wextra -static
+CFLAGS=-g -std=c99 -pedantic -Wall -Wextra -static -O1
 
 # Extra flags below
 #-Wfloat-equal -Wundef -Wstrict-prototypes -Wpointer-arith -Wstrict-overflow=5 -Waggregate-return -Wswitch-default -Wconversion -Wswitch-enum -Wunreachable-code
