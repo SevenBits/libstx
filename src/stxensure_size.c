4baca55b034b6bd8a5d0dbe8bfe9b554a83bec3b
Todd Gaunt <toddgaunt@protonmail.ch>
2017-03-17 09:17:58 +0000

    Remove power of two logic from stx_ensure_size

--- src/stxensure_size.c
+++ src/stxensure_size.c
@@ -5,19 +5,8 @@ int
 stxensure_size(stx *s1, size_t size)
 {
 	char *tmp;
-	size_t base;
 
-	base = 2;
-	if (size >= SIZE_MAX / 2) {
-		base = SIZE_MAX;
-	} else {
-		while (base < size) {
-			base <<= 1;
-		}
-	}
-
-	size = base;
-	if (size == s1->size)
+	if (s1->size >= size)
 		return 0;
 
 	tmp = realloc(s1->mem, size);
