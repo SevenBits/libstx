34da234379d1c827647f8d7969cef8642ad501df
Todd Gaunt <toddgaunt@protonmail.ch>
2017-04-02 04:35:06 -0400

    Add C++ wrapper header file. Includes namespacing and overloading

---
+++ libstx.hpp
@@ -0,0 +1,91 @@
+/* See LICENSE file for copyright and license details */
+
+/**
+ * This header is used to wrap the libstx library with a C++ namespace, and to
+ * change functions that had postfix types in their name to be overloaded
+ * functions.
+ */
+namespace libstx {
+	#include "libstx.h"
+
+	stx *alloc(stx *sp, size_t n)
+	{return stxalloc(sp, n);}
+
+	stx *del(stx *sp)
+	{return stxdel(sp);}
+
+	stx *grow(stx *sp, size_t n)
+	{return stxgrow(sp, n);}
+
+	stx *ensure_size(stx *sp, size_t n)
+	{return stxensure_size(sp, n);}
+
+	stx *valid(stx *sp)
+	{return stxvalid(sp);}
+
+	bool eq(const stx *s1, const stx *s2)
+	{return stxeq(s1, s2);}
+
+	void swap(stx *s1, stx *s2)
+	{return stxswap(s1, s2);}
+
+	stx *trunc(stx *sp, size_t len)
+	{return stxtrunc(sp, len);}
+
+	stx *cpy(stx *sp, const char *src, size_t len)
+	{return stxcpy_mem(sp, src, len);}
+
+	stx *cpy(stx *sp, const char *src)
+	{return stxcpy_str(sp, src);}
+
+	stx *cpy(stx *sp, const stx *src)
+	{return stxcpy_stx(sp, src);}
+
+	stx *ins(stx *sp, size_t pos, const char *src, size_t len)
+	{return stxins_mem(sp, pos, src, len);}
+
+	stx *ins(stx *sp, size_t pos, const char *src)
+	{return stxins_str(sp, pos, src);}
+
+	stx *ins(stx *sp, size_t pos, uint32_t wc)
+	{return stxins_uni(sp, pos, wc);}
+
+	stx *ins(stx *sp, size_t pos, const stx *src)
+	{return stxins_stx(sp, pos, src);}
+
+	stx *app(stx *sp, const char *src, size_t len)
+	{return stxapp_mem(sp, src, len);}
+
+	stx *app(stx *sp, const char *src)
+	{return stxapp_str(sp, src);}
+
+	stx *app(stx *sp, uint32_t wc)
+	{return stxapp_uni(sp, wc);}
+
+	stx *app(stx *sp, const stx *src)
+	{return stxapp_stx(sp, src);}
+
+	stx *find(stx *slice, stx *sp, const char *src, size_t len)
+	{return stxfind_mem(slice, sp, src, len);}
+
+	stx *find(stx *slice, stx *sp, const char *src)
+	{return stxfind_str(slice, sp, src);}
+
+	stx *find(stx *slice, stx *sp, const stx *src)
+	{return stxfind_stx(slice, sp, src);}
+
+	stx *slice(stx *slice, stx *sp, size_t begin, size_t end)
+	{return stxslice(slice, sp, begin, end);}
+
+	stx *rstrip(stx *sp, const char *chs, size_t len)
+	{return stxrstrip(sp, chs, len);}
+
+	stx *lstrip(stx *sp, const char *chs, size_t len)
+	{return stxlstrip(sp, chs, len);}
+
+	stx *strip(stx *sp, const char *chs, size_t len)
+	{return stxstrip(sp, chs, len);}
+
+	int uni8f32(char *dst, uint32_t wc)
+	{return stxuni8f32(dst, wc);}
+}
