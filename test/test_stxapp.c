a9af2cd75317e7e1eeb7cf187ac9cc5103acd736
Todd Gaunt <toddgaunt@protonmail.ch>
2017-05-10 13:34:06 -0400

    Shorten name of byte-arrays

--- test/test_stxapp.c
+++ test/test_stxapp.c
@@ -4,41 +4,59 @@
 #include "../libstx.h"
 #include "test.h"
 
-#define bytes1 "--testbytes\0\nbc\rfghij\12\0\0\n"
-#define bytes2 "hello\0world\n9"
+#define b1 "--testbytes\0\nbc\rfghij\12\0\0\n"
+#define b2 "hello\0world\n9"
 
-TEST_DEFINE(stxapp_mem_empty_to_full)
+TEST_DEFINE(stxapp_mem_empty)
 {
 	stx s1;
-	stxalloc(&s1, sizeof(bytes1));
+	stxalloc(&s1, sizeof(b1));
 
-	TEST_ASSERT(&s1 == stxapp_mem(&s1, bytes1, sizeof(bytes1)));
+	TEST_ASSERT(&s1 == stxapp_mem(&s1, b1, sizeof(b1)));
 	TEST_ASSERT(s1.mem != NULL);
-	TEST_ASSERT(0 == memcmp(s1.mem, bytes1, sizeof(bytes1)));
-	TEST_ASSERT(sizeof(bytes1) == s1.len);
-	TEST_ASSERT(sizeof(bytes1) == s1.size);
+	TEST_ASSERT(0 == memcmp(s1.mem, b1, sizeof(b1)));
+	TEST_ASSERT(sizeof(b1) == s1.len);
+	TEST_ASSERT(sizeof(b1) == s1.size);
 
 	stxfree(&s1);
 
 	TEST_END;
 }
 
-TEST_DEFINE(stxapp_mem_concatenate)
+TEST_DEFINE(stxapp_mem_bytes_seperately)
 {
 	stx s1;
-	char tstr[sizeof(bytes1) + sizeof(bytes2)];
+	stxalloc(&s1, sizeof(b1));
 
-	memcpy(tstr, bytes1, sizeof(bytes1));
-	memcpy(tstr + sizeof(bytes1), bytes2, sizeof(bytes2));
-	const size_t tlen = sizeof(bytes1) + sizeof(bytes2);
+	for (size_t i=0; i<sizeof(b1); ++i) {
+		TEST_ASSERT(&s1 == stxapp_mem(&s1, &b1[i], 1));
+		TEST_ASSERT(s1.mem != NULL);
+		TEST_ASSERT(0 == memcmp(s1.mem, b1, i + 1));
+		TEST_ASSERT(i + 1 == s1.len);
+		TEST_ASSERT(sizeof(b1) == s1.size);
+	}
 
-	stxalloc(&s1, sizeof(bytes1) + sizeof(bytes2));
+	stxfree(&s1);
 
-	TEST_ASSERT(&s1 == stxapp_mem(&s1, bytes1, sizeof(bytes1)));
-	TEST_ASSERT(sizeof(bytes1) == s1.len);
+	TEST_END;
+}
+
+TEST_DEFINE(stxapp_mem_twice)
+{
+	stx s1;
+	char tstr[sizeof(b1) + sizeof(b2)];
+
+	memcpy(tstr, b1, sizeof(b1));
+	memcpy(tstr + sizeof(b1), b2, sizeof(b2));
+	const size_t tlen = sizeof(b1) + sizeof(b2);
+
+	stxalloc(&s1, sizeof(b1) + sizeof(b2));
+
+	TEST_ASSERT(&s1 == stxapp_mem(&s1, b1, sizeof(b1)));
+	TEST_ASSERT(sizeof(b1) == s1.len);
 	TEST_ASSERT(tlen == s1.size);
 
-	TEST_ASSERT(&s1 == stxapp_mem(&s1, bytes2, sizeof(bytes2)));
+	TEST_ASSERT(&s1 == stxapp_mem(&s1, b2, sizeof(b2)));
 	TEST_ASSERT(s1.mem != NULL);
 	TEST_ASSERT(0 == memcmp(s1.mem, tstr, tlen));
 	TEST_ASSERT(tlen == s1.len);
@@ -49,56 +67,38 @@ TEST_DEFINE(stxapp_mem_concatenate)
 	TEST_END;
 }
 
-TEST_DEFINE(stxapp_mem_bytes_seperately)
-{
-	stx s1;
-	stxalloc(&s1, sizeof(bytes1));
-
-	for (size_t i=0; i<sizeof(bytes1); ++i) {
-		TEST_ASSERT(&s1 == stxapp_mem(&s1, &bytes1[i], 1));
-		TEST_ASSERT(s1.mem != NULL);
-		TEST_ASSERT(0 == memcmp(s1.mem, bytes1, i + 1));
-		TEST_ASSERT(i + 1 == s1.len);
-		TEST_ASSERT(sizeof(bytes1) == s1.size);
-	}
-
-	stxfree(&s1);
-
-	TEST_END;
-}
-
 TEST_DEFINE(stxapp_str_empty)
 {
 	stx s1;
-	stxalloc(&s1, strlen(bytes1));
+	stxalloc(&s1, strlen(b1));
 
-	TEST_ASSERT(&s1 == stxapp_str(&s1, bytes1));
+	TEST_ASSERT(&s1 == stxapp_str(&s1, b1));
 	TEST_ASSERT(s1.mem != NULL);
-	TEST_ASSERT(0 == memcmp(s1.mem, bytes1, strlen(bytes1)));
-	TEST_ASSERT(strlen(bytes1) == s1.len);
-	TEST_ASSERT(strlen(bytes1) == s1.size);
+	TEST_ASSERT(0 == memcmp(s1.mem, b1, strlen(b1)));
+	TEST_ASSERT(strlen(b1) == s1.len);
+	TEST_ASSERT(strlen(b1) == s1.size);
 
 	stxfree(&s1);
 
 	TEST_END;
 }
 
-TEST_DEFINE(stxapp_str_concatenate)
+TEST_DEFINE(stxapp_str_twice)
 {
 	stx s1;
-	char tstr[strlen(bytes1) + strlen(bytes2)];
+	char tstr[strlen(b1) + strlen(b2)];
 
-	strcpy(tstr, bytes1);
-	strcpy(tstr + strlen(bytes1), bytes2);
+	strcpy(tstr, b1);
+	strcpy(tstr + strlen(b1), b2);
 	const size_t tlen = strlen(tstr);
 
-	stxalloc(&s1, strlen(bytes1) + strlen(bytes2));
+	stxalloc(&s1, strlen(b1) + strlen(b2));
 
-	TEST_ASSERT(&s1 == stxapp_str(&s1, bytes1));
-	TEST_ASSERT(strlen(bytes1) == s1.len);
+	TEST_ASSERT(&s1 == stxapp_str(&s1, b1));
+	TEST_ASSERT(strlen(b1) == s1.len);
 	TEST_ASSERT(tlen == s1.size);
 
-	TEST_ASSERT(&s1 == stxapp_str(&s1, bytes2));
+	TEST_ASSERT(&s1 == stxapp_str(&s1, b2));
 	TEST_ASSERT(s1.mem != NULL);
 	TEST_ASSERT(0 == memcmp(s1.mem, tstr, tlen));
 	TEST_ASSERT(tlen == s1.len);
@@ -113,10 +113,10 @@ int
 main(void)
 {
 	TEST_INIT(ts);
-	TEST_RUN(ts, stxapp_mem_empty_to_full);
-	TEST_RUN(ts, stxapp_mem_concatenate);
+	TEST_RUN(ts, stxapp_mem_empty);
 	TEST_RUN(ts, stxapp_mem_bytes_seperately);
+	TEST_RUN(ts, stxapp_mem_twice);
 	TEST_RUN(ts, stxapp_str_empty);
-	TEST_RUN(ts, stxapp_str_concatenate);
+	TEST_RUN(ts, stxapp_str_twice);
 	TEST_PRINT(ts);
 }
