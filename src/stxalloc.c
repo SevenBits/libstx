50847821bd0f400cad3cc58d29ddbcd154736a22
Todd Gaunt <toddgaunt@protonmail.ch>
2017-05-09 20:43:14 -0400

    Refactor to no longer depend on stxgrow so behavior is correct

--- src/stxalloc.c
+++ src/stxalloc.c
@@ -4,9 +4,20 @@
 int
 stxalloc(stx *sp, size_t n)
 {
-	memset(sp, 0, sizeof(*sp));
-	if (stxgrow(sp, n))
+	sp->len = 0;
+
+	// Always set the memory location to NULL, don't let the implementation
+	// of malloc decide what to do.
+	if (0 == n) {
+		sp->mem = NULL; 
+		sp->size = 0;
+		return 0;
+	}
+
+	if (!(sp->mem = malloc(n)))
 		return -1;
+
+	sp->size = n;
 
 	return 0;
 }
