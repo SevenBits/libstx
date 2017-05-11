036694472e24015b254b3452b0ae502dcb275f0a
Todd Gaunt <toddgaunt@protonmail.ch>
2017-05-11 17:31:58 -0400

    Fix test message and compatibility with sh

--- test/run_all.sh
+++ test/run_all.sh
@@ -1,13 +1,14 @@
-#!/usr/bin/env sh
+#!//bin/sh
+# This script runs all tests until a non-zero return is encountered.
+# Returns if all tests ran successfully, -1 otherwise.
 
 TEST_DIR=$(dirname $(realpath $0))
 
-# Run all tests until a non-zero return is encountered.
 for i in "$TEST_DIR"/*.test; do
-	printf -- "%0.s-" {1..79}
-	printf "$i\n"
+	printf -- "%0.s-" {1..78}
+	printf -- "\n$(basename $i)\n"
 	"$i"
-	if ! [[ $? -eq 0 ]]; then
+	if ! [ $? -eq 0 ]; then
 		exit -1
 	fi
 done
